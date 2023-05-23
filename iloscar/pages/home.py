c5a6aef9781bd4306089bc333ca70f8bc995ef8a
Shihan Li <57557675+Shihan150@users.noreply.github.com>
2023-05-23 17:58:57 -0500

    Update home.py

--- iloscar/pages/home.py
+++ iloscar/pages/home.py
@@ -5,14 +5,17 @@ from style import *
 dash.register_page(__name__, path='/', order = 1)
 
 link = "https://doi.org/10.5194/gmd-5-149-2012"
+link1 = 'https://github.com/Shihan150/iloscar/blob/main/README.md#example'
 layout = html.Div(children=[
     html.Div(style = Home_STYLE, children =[
     html.P("The LOSCAR model is designed to efficiently compute the partitioning of carbon between ocean, atmosphere, and sediments on time scales ranging from centuries to millions of years. "),
     html.Br(),
     html.P(["For a detailed model description, please refer to ",
-            html.Label([ '',html.A('Zeebe (2012)', href=link)])]),
+            html.Label([ '',html.A('Zeebe (2012)', href=link, target = '_blank')])]),
     html.Br(),
     html.P("This is the iLOSCAR-0.1 written in Python, which allows users to run the model from the website interface."),
+    html.Br(),
+    html.P([html.Label([ html.A('Examples', href=link1, target = '_blank'),  '']), ' ', ' are provided in the tutorial for a quick start .']),
     html.Br(),
     html.P("Author"),
     html.P(" Shihan Li | Department of Oceanography, Texas A&M University, College Station, Texas, 77843, USA"),
