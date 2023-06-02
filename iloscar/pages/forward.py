0b6701ea74ff0b4c72504ac73df1d73fbab6bee2
Shihan Li <57557675+Shihan150@users.noreply.github.com>
2023-06-02 15:41:15 -0500

    Update forward.py

--- iloscar/pages/forward.py
+++ iloscar/pages/forward.py
@@ -281,7 +281,7 @@ layout = dbc.Container(fluid = False,style = Home_STYLE, children = [
     dcc.Store(id = 'tsol', data = []),
 
     dbc.Row([
-        dbc.Col(html.Div("The first column in each table is editable, allowing users to manually adjust parameters.",
+        dbc.Col(html.Div("The first column in each table is editable and users can manually adjust the values.",
                         style={'fontSize':20, 'textAlign':'left', 'font-style': 'italic', "margin-left": "10rem",'padding': '1rem 1rem'} )),
 
     ]),
